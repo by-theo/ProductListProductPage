ed32a2b48da65bec31f6abba72af6b2e31b7f21c
Theo Taylor <theotaylor@taylord.tech>
2019-02-02 14:46:15 -0400

    Update App.js
    Fully responsive product list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import { Dimensions, ScrollView, StyleSheet, View } from "react-native";
+import Columns from "react-columns";
 
 const mobileMaxWidth = 600;
 const fullWidth = 900;
-const sidebarWidth = 300;
+
+// For Product list
+const gap = 8.5;
 
 class App extends Component {
   constructor(props) {
@@ -37,14 +40,31 @@ class App extends Component {
     const { width } = dims.window;
     const isMobile = mobileMaxWidth >= width;
     const isFullWidth = fullWidth <= width;
+    const sidebarWidth = isFullWidth ? (width - fullWidth) / 3 + 300 : 300;
+
+    // For Product list
+    let columns = 2;
+    let itemSide;
+
+    if (isFullWidth) {
+      itemSide = (sidebarWidth - gap * 3) / columns;
+    } else {
+      // Handle funky issue where there is no right padding
+      let bodyWidth = width - gap * 3;
+      itemSide = (bodyWidth - gap * 3) / columns;
+    }
+
     return {
       isFullWidth,
-      isMobile
+      isMobile,
+      itemSide,
+      columns,
+      sidebarWidth
     };
   }
 
   renderProduct() {
-    const { isFullWidth, isMobile } = this.state;
+    const { isFullWidth, isMobile, sidebarWidth } = this.state;
     const { width } = this.state.window;
     const productContainerWidth = 600;
     const styles = StyleSheet.create({
@@ -52,7 +72,7 @@ class App extends Component {
         width: isFullWidth ? width - sidebarWidth : "100%",
         alignItems: "center",
         backgroundColor: "blue",
-        paddingVertical: 120
+        paddingVertical: 144
       },
       productContainer: {
         width: isMobile ? productContainerWidth / 2 : productContainerWidth,
@@ -82,17 +102,65 @@ class App extends Component {
   }
 
   renderProductList() {
-    const { isFullWidth } = this.state;
-    const { height, width } = this.state.window;
-    const containerWidth = isFullWidth ? 300 : width;
+    const { isFullWidth, sidebarWidth } = this.state;
+    const { width } = this.state.window;
+    const containerWidth = isFullWidth ? sidebarWidth : width;
     const styles = StyleSheet.create({
       container: {
-        width: containerWidth,
-        height: height,
-        backgroundColor: "pink"
+        width: containerWidth
       }
     });
-    return <View style={styles.container} />;
+    return <View style={styles.container}>{this.renderColumns()}</View>;
+  }
+
+  renderItems(style) {
+    const itemViews = [];
+    const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    for (let i = 0; i < items.length; i++) {
+      itemViews.push(<View key={i} style={style} />);
+    }
+    return itemViews;
+  }
+
+  renderColumns() {
+    const { columns, itemSide, isFullWidth } = this.state;
+    const { height } = this.state.window;
+    const fullWidthBody = isFullWidth ? { height } : {};
+    const styles = StyleSheet.create({
+      body: {
+        ...fullWidthBody,
+        marginLeft: isFullWidth ? gap + "px" : gap * 1.5 + "px",
+        // Handle funky issue where there is no right padding
+        marginRight: isFullWidth ? "0px" : gap * 2 + "px",
+        alignItems: "center"
+      },
+      content: {
+        flex: 1
+      },
+      contentContainer: {
+        paddingTop: gap * 2,
+        paddingRight: gap
+      },
+      item: {
+        width: itemSide,
+        height: itemSide,
+        backgroundColor: "tomato",
+        marginBottom: isFullWidth ? gap : gap * 2
+      }
+    });
+    return (
+      <View style={styles.body}>
+        <ScrollView
+          style={styles.content}
+          contentContainerStyle={styles.contentContainer}
+          showsVerticalScrollIndicator={false}
+        >
+          <Columns columns={columns} gap={gap + "px"}>
+            {this.renderItems(styles.item)}
+          </Columns>
+        </ScrollView>
+      </View>
+    );
   }
 
   render() {
@@ -100,18 +168,15 @@ class App extends Component {
     const styles = StyleSheet.create({
       app: {
         flex: 1,
-        flexDirection: isFullWidth ? "row" : "column",
-        backgroundColor: "red"
+        flexDirection: isFullWidth ? "row" : "column"
       }
     });
     if (isFullWidth) {
       return (
-        <ScrollView>
-          <View style={styles.app}>
-            {this.renderProductList()}
-            {this.renderProduct()}
-          </View>
-        </ScrollView>
+        <View style={styles.app}>
+          {this.renderProductList()}
+          {this.renderProduct()}
+        </View>
       );
     } else {
       return (
